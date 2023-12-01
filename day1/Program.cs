b6d8b0d8065dfb1416c41de3f2a57ce0b8cf0aa4
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-12-01 06:33:37 +0000

    d1p1

---
+++ day1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var input = await System.IO.File.ReadAllLinesAsync(args[0]);
+
+var sum = 0;
+
+var regexDigit = new Regex(@"\d");
+
+foreach (var line in input)
+{
+    var foo = from c in line
+              where regexDigit.Match(c.ToString()).Success
+              select c;
+
+    sum += int.Parse(new string([foo.First(), foo.Last()]));
+}
+
+Console.WriteLine(sum);
