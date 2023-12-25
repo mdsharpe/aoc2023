6c866f3f0089d2311ff0ecaf9c6336248f459120
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-12-25 13:26:41 +0000

    d25p1 wip

--- day25/Program.cs
+++ day25/Program.cs
@@ -1,36 +1,36 @@
 ﻿var input = await File.ReadAllLinesAsync(args[0]);
 var connections = Parser.Parse(input);
 
-/**
- * Traverse connections
- */
-
-for (int s0 = 0; s0 < connections.Length; s0++)
-{
-    for (int s1 = 0; s1 < connections.Length; s1++)
+Parallel.For(
+    0,
+    connections.Length,
+    new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
+    (s0) =>
     {
-        for (int s2 = 0; s2 < connections.Length; s2++)
+        var visitor = new Visitor();
+
+        for (var s1 = 0; s1 < connections.Length; s1++)
         {
-            if (s0 == s1 || s0 == s2 || s1 == s2)
+            for (var s2 = 0; s2 < connections.Length; s2++)
             {
-                continue;
-            }
-
-            var connectionsExceptSevered = connections
-                .Except([connections[s0], connections[s1], connections[s2]]);
-
-            var visitor = new Visitor();
-            visitor.Visit(connectionsExceptSevered);
-            Console.WriteLine($"Severing {s0},{s1},{s2} => found {visitor.Groups.Count} groups");
-            if (visitor.Groups.Count == 2)
-            {
-                foreach (var g in visitor.Groups)
+                if (!(s0 == s1 || s0 == s2 || s1 == s2))
                 {
-                    Console.WriteLine(string.Join(",", g.Count));
-                }
+                    var connectionsExceptSevered = connections
+                        .Except([connections[s0], connections[s1], connections[s2]]);
 
-                Environment.Exit(0);
+                    visitor.Clear();
+                    visitor.Visit(connectionsExceptSevered);
+
+                    if (visitor.Groups.Count == 2)
+                    {
+                        foreach (var g in visitor.Groups)
+                        {
+                            Console.WriteLine(string.Join(",", g.Count));
+                        }
+
+                        Environment.Exit(0);
+                    }
+                }
             }
         }
-    }
-}
+    });
--- day25/Visitor.cs
+++ day25/Visitor.cs
@@ -2,6 +2,11 @@ class Visitor
 {
     public List<HashSet<string>> Groups { get; } = [];
 
+    public void Clear()
+    {
+        Groups.Clear();
+    }
+
     public void Visit(IEnumerable<Connection> connections)
     {
         foreach (var c in connections)
